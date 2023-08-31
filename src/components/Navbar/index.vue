28f7f8ec6008a3aad68e07857e5cd0328da66c7f
Pedro Machado <macpedrot@gmail.com>
2023-08-31 20:45:15 +0100

    ✨ add navbar component

---
+++ src/components/Navbar/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav class="h-full flex flex-col gap-4 bg-red-700 max-w-sm box-border p-8">
+        <div>Logo</div>
+        <ul class="flex flex-col gap-3">
+            <li v-for="(tab, index) in navTabs" :key="tab.name"
+                :class="['capitalize', 'text-lg', 'text-slate-100', 'hover:bg-red-600', 'cursor-pointer', 'p-2', 'rounded-md', index === activeTab && 'bg-red-600']"
+                @click="changeActiveTab(index)">
+                {{ tab.name }}
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            navTabs: [
+                {
+                    name: 'pokemons',
+                    link: ''
+                },
+                {
+                    name: 'Badges',
+                    link: '',
+                },
+            ],
+            activeTab: 0,
+        }
+    },
+
+    methods: {
+        changeActiveTab(tab) {
+            this.activeTab = tab
+        }
+    }
+
+}
+</script>
+
+<style></style>
