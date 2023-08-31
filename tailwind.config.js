9e3505596a31d2641217b778f66122388be46171
Pedro Machado <macpedrot@gmail.com>
2023-08-31 21:52:36 +0100

    💄 add config value

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
       extend: {},
+      minWidth: {
+        '240': '240px',
+      }
     },
     variants: {
       extend: {},
