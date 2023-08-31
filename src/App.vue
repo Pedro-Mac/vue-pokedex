badaecbc6729e132af9a4dc7d4359e60db71ec65
Pedro Machado <macpedrot@gmail.com>
2023-08-31 22:05:01 +0100

    ✨ render pokemon list

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="min-h-screen h-screen">
-    <NavBar />
+  <div class="h-screen flex overflow-scroll ">
+    <NavBar :nav-tabs="navTabs" :active-tab="activeTab" />
+    <main class="flex-1 overflow-scroll">
+      <PokemonList />
+    </main>
   </div>
 </template>
 <script>
 import NavBar from './components/NavBar/index.vue';
+import PokemonList from './views/PokemonList.vue';
 
 export default {
+  data() {
+    return {
+      navTabs: [
+        {
+          name: 'pokemons',
+          link: ''
+        }
+      ],
+      activeTab: 0,
+    }
+  },
   components: {
-    NavBar
+    NavBar,
+    PokemonList
   },
 }
 </script>
--- src/components/Navbar/index.vue
+++ src/components/Navbar/index.vue
@@ -1,6 +1,5 @@
 <template>
-    <nav class="h-full flex flex-col gap-4 bg-red-700 max-w-sm box-border p-8">
-        <div>Logo</div>
+    <nav class="h-full flex flex-col gap-4 bg-red-700 max-w-sm box-border p-8 min-w-240">
         <ul class="flex flex-col gap-3">
             <li v-for="(tab, index) in navTabs" :key="tab.name"
                 :class="['capitalize', 'text-lg', 'text-slate-100', 'hover:bg-red-600', 'cursor-pointer', 'p-2', 'rounded-md', index === activeTab && 'bg-red-600']"
@@ -13,28 +12,13 @@
 
 <script>
 export default {
-    data() {
-        return {
-            navTabs: [
-                {
-                    name: 'pokemons',
-                    link: ''
-                },
-                {
-                    name: 'Badges',
-                    link: '',
-                },
-            ],
-            activeTab: 0,
-        }
-    },
-
     methods: {
         changeActiveTab(tab) {
             this.activeTab = tab
-        }
-    }
+        },
+    },
 
+    props: ['navTabs', 'activeTab']
 }
 </script>
 
