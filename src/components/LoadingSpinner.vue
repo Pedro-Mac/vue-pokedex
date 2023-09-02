7b6035601d18d4d812ec4d5b441739050e06e7b5
Pedro Machado <macpedrot@gmail.com>
2023-09-02 15:53:33 +0100

    💄 add loading component

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="loader">
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.loader {
+    width: 60px;
+    height: 60px;
+    border: 5px solid rgb(185 28 28);
+    border-bottom-color: transparent;
+    border-radius: 50%;
+    display: inline-block;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
