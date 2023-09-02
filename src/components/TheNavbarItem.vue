c84a9b60139aade8a97db073f2af412f6b8d0c77
Pedro Machado <macpedrot@gmail.com>
2023-09-02 22:09:32 +0100

    update style condition

--- src/components/TheNavbarItem.vue
+++ src/components/TheNavbarItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <li :class="['capitalize', 'text-lg', 'text-slate-100', 'hover:bg-slate-100', 'hover:text-red-700', 'cursor-pointer', 'p-2', 'rounded-md', isActiveTab && 'bg-white', isActiveTab && 'text-red-700']"
+    <li :class="['capitalize', 'text-lg', 'hover:bg-slate-100', 'hover:text-red-700', 'cursor-pointer', 'p-2', 'rounded-md', isActiveTab && 'bg-white', isActiveTab ? 'text-red-700' : 'text-slate-100']"
         @click="changeActiveTab(placement - 1)">
         <span>{{ placement }}. {{ tab.name }}</span>
     </li>
